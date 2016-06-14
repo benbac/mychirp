cd114ed7d2d375c48e44afec3ce3d3e24d29df6b
Ben <benbac@gmail.com>
2016-06-14 16:20:20 -0400

    adding module5 and gitignore of node_modules

---
+++ module5/Start/public/javascripts/chirpApp.js
@@ -0,0 +1,76 @@
+//chirpApp.js
+var app = angular.module('chirpApp', ['ngRoute', 'ngResource']).run(function($rootScope, $http) {
+	$rootScope.authenticated = false;
+	$rootScope.current_user = '';
+	
+	$rootScope.logout = function(){
+		$http.get('/auth/sigout');
+		$rootScope.authenticated = false;
+		$rootScope.current_user = '';
+	};
+});
+
+app.config(function($routeProvider){
+  $routeProvider
+    //the timeline display
+    .when('/', {
+      templateUrl: 'main.html',
+      controller: 'mainController'
+    })
+    //the login display
+    .when('/login', {
+      templateUrl: 'login.html',
+      controller: 'authController'
+    })
+    //the signup display
+    .when('/register', {
+      templateUrl: 'register.html',
+      controller: 'authController'
+    });
+});
+
+
+app.factory('postService', function($resource){
+	return $resource('/api/posts/:id');
+});
+
+
+
+app.controller('mainController', function($rootScope, $scope, postService){
+	$scope.posts = postService.query();
+	$scope.newPost = {created_by:'', text:'', created_at:''};
+	
+	$scope.post = function(){
+		$scope.newPost.created_by = $rootScope.current_user;
+		$scope.newPost.created_at = Date.now();
+		postService.save($scope.newPost, function(){
+			$scope.posts = postService.query();
+			$scope.newPost = {created_by:'', text:'', created_at:''};
+		});
+	};
+});
+
+app.controller('authController', function($scope, $http, $rootScope, $location){
+	$scope.user = {username: '', password: ''};
+	$scope.error_message = '';
+
+	$scope.login = function(){
+		$http.post('/auth/login', $scope.user).success(function(data){
+			$rootScope.authenticated = true;
+			$rootScope.current_user = data.user.username;
+			$location.path('/');
+		});
+		//placeholder until authentication is implemented
+		$scope.error_message = 'login request for ' + $scope.user.username;
+	};
+
+	$scope.register = function(){
+		$http.post('/auth/signup', $scope.user).success(function(data){
+			$rootScope.authenticated = true;
+			$rootScope.current_user = data.user.username;
+			$location.path('/');
+		});		
+		//placeholder until authentication is implemented
+		$scope.error_message = 'registeration request for ' + $scope.user.username;
+	};
+});
