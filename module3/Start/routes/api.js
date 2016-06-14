1779650007009508805eab81d5a48defa68c8749
Benedict Bacayon <benbac@gmail.com>
2016-06-13 23:07:37 -0400

    Updated module3

--- module3/Start/routes/api.js
+++ module3/Start/routes/api.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function(req, res, next){
+	
+	if(req.method === "GET"){
+		//continue to the next middleware or request handler
+		return next();
+	}
+	
+	if(!req.isAuthenticated()){
+		//user not authenticated, redirect to login page
+		return res.redirect('/#login');
+	}
+	
+	return next();
+});
+
 router.route('/posts')
 	//return all posts
 	.get(function(req, res){
@@ -27,3 +42,27 @@ router.route('/posts/:id')
 	})
 
 module.exports = router;
+
+
+/**
+//Used for routes that must be authenticated.
+function isAuthenticated (req, res, next) {
+    // if user is authenticated in the session, call the next() to call the next request handler 
+    // Passport adds this method to request object. A middleware is allowed to add properties to
+    // request and response objects
+
+    //allow all get request methods
+    if(req.method === "GET"){
+        return next();
+    }
+    if (req.isAuthenticated()){
+        return next();
+    }
+
+    // if the user is not authenticated then redirect him to the login page
+    return res.redirect('/#login');
+};
+
+//Register the authentication middleware
+router.use('/posts', isAuthenticated);
+**/
